bbe424d7f3fd47cb6057a5f1f6628f9a4147016a
Vladislav <MonsterLPchanel@gmail.com>
2023-04-23 07:53:34 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+const isProduction = process.env.NODE_ENV === 'prod';
+
 export default defineConfig({
   plugins: [react()],
+  base: isProduction ? '/calculator/' : '/',
 });
